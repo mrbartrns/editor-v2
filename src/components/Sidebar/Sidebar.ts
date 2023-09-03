311568ceda05c40ae046d19faed80a51b296f870
Junhyuck <mrbartrns@naver.com>
2023-09-03 14:57:10 +0900

    Sidebar 구현

---
+++ src/components/Sidebar/Sidebar.ts
@@ -0,0 +1,17 @@
+import { Component } from "..";
+
+interface Node<T> {
+  nodeId: string;
+  data: T | null
+  children?: Node<T> | null
+}
+
+interface State {
+  root: 
+}
+
+class Sidebar extends Component {
+  setup() {}
+}
+
+export default Sidebar;
